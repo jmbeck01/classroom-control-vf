6157377d64a6bcf70aed59f6fd5d7d5fa8cd2e70
jmbeck01 <jmbeck01@gmail.com>
2016-07-28 11:28:47 -0500

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,9 +1,9 @@
 class users::admins {
-  users::managed_user { 'jose' }
-  users::managed_user { 'alice'
+  users::managed_user { 'jose': }
+  users::managed_user { 'alice':
   group => admin,
   }
-  users::managed_user { 'chen' } 
+  users::managed_user { 'chen': } 
   group { 'admin':
     ensure => present,
     }
