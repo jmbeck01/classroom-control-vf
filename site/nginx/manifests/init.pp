cb0d4f7b63955225d86f0a90895dac5628b28734
jmbeck01 <jmbeck01@gmail.com>
2016-07-28 08:47:04 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,23 +1,22 @@
 class nginx {
 
+  file {
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+  }
   package { 'nginx':
     ensure => 'present',
   }
 
   file { '/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify => Service['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
   }
