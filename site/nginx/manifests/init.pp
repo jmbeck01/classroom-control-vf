3f534820e003692b66e1228928d776269e0be93e
jmbeck01 <jmbeck01@gmail.com>
2016-07-29 10:54:03 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,45 +2,6 @@ class nginx(
 $root = undef,
 ){
 
-case $::osfamily {
-  'debian': {
-    $package_name = 'nginx'
-    $file_owner = 'root'
-    $file_group = 'root'
-    $doc_root = $root
-    $config_dir = '/etc/nginx'
-    $server_block_dir = '/etc/nginx/conf.d'
-    $log_dir = '/var/log/nginx'
-    $service_name = 'nginx'
-    $runas_user = 'www-data'
-  }
-  'windows': {
-    $package_name = 'nginx-service'
-    $file_owner = 'Administrators'
-    $file_group = 'Administrators'
-    $doc_root = $root
-    $config_dir = 'C:/ProgramData/nginx'
-    $server_block_dir = 'C:/ProgramData/nginx/conf.d'
-    $log_dir = 'C:/ProgramData/nginx/logs'
-    $service_name = 'nginx'
-    $runas_user = 'nobody'
-  }
-  'redhat': {
-    $package_name = 'nginx'
-    $file_owner = 'root'
-    $file_group = 'root'
-    $doc_root = $root
-    $config_dir = '/etc/nginx'
-    $server_block_dir = '/etc/nginx/conf.d'
-    $log_dir = '/var/log/nginx'
-    $service_name = 'nginx'
-    $runas_user = 'nginx'
-  }
-  default: {
-      fail("Operating system #{operatingsystem} is not supported.")
-  }
-}
-
   File {
     owner => 'root',
     group => 'root',
